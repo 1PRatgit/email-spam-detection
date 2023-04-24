452ab67ef14dfa2c3d8b4757c1d647330e5dc239
Pranjali <rrpranjali@gmail.com>
2023-04-24 13:51:46 +0530

    download stopwords

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ import string
 from nltk.corpus import stopwords
 import nltk
 nltk.download('punkt')
+nltk.download('stopwords')
 from nltk.stem.porter import PorterStemmer
 import numpy as np
 # from streamlit-lottie import st_lottie
